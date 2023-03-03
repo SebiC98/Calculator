1286568e3db8ff72f7f47e04ac9ef06143fc30a2
SebiC98 <sebastiancioata98@gmail.com>
2023-03-03 15:29:07 +0200

    First commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sebastian Cioată on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
